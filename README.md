a5e3bd2336d086e50d749b20c07b94bbb7c562c2
rayabergin <rayaberginwrites@gmail.com>
2022-02-02 18:26:35 -0800

    added atl images favicon google fonts sticky nav with js css reset color var revised comments and line spaces footer readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 turning an HTML page and content into a nice looking fast food restaurant single page website using CSS Flexbox
 
 responsive for tablets and mobile devices
+
+former project name COSW20-Lab4: Flexburgers Starter
---
+++ myscripts.js
@@ -0,0 +1,19 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+	myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add sticky class to the navbar when it reaches its scroll position. Remove sticky when navbar leaves scroll position
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
+	}
+}
